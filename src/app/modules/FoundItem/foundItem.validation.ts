f03b8b9431db0e52735967cfb5a56ba4eb21f77f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-05-25 09:26:20 +0600

    added claim process in lost item

--- src/app/modules/FoundItem/foundItem.validation.ts
+++ src/app/modules/FoundItem/foundItem.validation.ts
@@ -5,18 +5,22 @@ const reportFoundItem = z.object({
     invalid_type_error: "Category ID must be a string.",
     required_error: "Category ID is required.",
   }),
+
   foundItemName: z.string({
     invalid_type_error: "Found Item Name must be a string.",
     required_error: "Found Item Name is required.",
   }),
+
   description: z.string({
     invalid_type_error: "Description must be a string.",
     required_error: "Description is required.",
   }),
+
   location: z.string({
     invalid_type_error: "Location must be a string.",
     required_error: "Location is required.",
   }),
+
   foundDate: z
     .string({
       invalid_type_error: "Lost Date must be a string.",
@@ -32,6 +36,12 @@ const reportFoundItem = z.object({
   brand: z
     .string({
       invalid_type_error: "Brand must be a string.",
+    })
+    .optional(),
+
+  claimProcess: z
+    .string({
+      invalid_type_error: "Claim Process must be a string.",
     })
     .optional(),
 });
